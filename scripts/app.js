d67083ec1c6d6a229479fcaddafc9831e0f416a1
Adrienne Cabouet <adriennecabouet@Adriennes-MacBook-Pro.local>
2018-01-29 14:21:46 -0700

    ntc library to get names of colors from hex codes

--- scripts/app.js
+++ scripts/app.js
@@ -2,16 +2,26 @@ require.config({
     baseUrl: 'scripts/lib',
     paths: {
         app: '../app',
-        jquery: 'vendor/jquery.min'
+        jquery: 'vendor/jquery.min',
+        ntc: 'vendor/ntc'
     }
 });
 
-require(['jquery', 'wdf/widget-config'], function($, WidgetConfig) {
+require(['jquery', 'wdf/widget-config', 'ntc'], function($, WidgetConfig) {
 
     // load the widget configuration
     var config = new WidgetConfig();
+    var n_match;
     config.on('config-initialized', function(event, data) {
+
+        // For 1st Box
         $('#group1').css('background', config.preferences['Color 1']);
+
+        // Get name of color preference from hex-code
+        n_match = ntc.name(config.preferences['Color 1']);
+        $('#group1').find('span.color').text(n_match[1]);
+
+
     });
     config.on('config-error', function() {
         $('#group1').text('Error loading preferences');
