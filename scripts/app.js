4d8a5da34eb639ef96a043e8a12a2a012e3df6bc
Adrienne Cabouet <adriennecabouet@Adriennes-MacBook-Pro.local>
2018-01-29 15:04:26 -0700

    Pick color at random and apply it to a div working

--- scripts/app.js
+++ scripts/app.js
@@ -11,20 +11,33 @@ require(['jquery', 'wdf/widget-config', 'ntc'], function($, WidgetConfig) {
 
     // load the widget configuration
     var config = new WidgetConfig();
-    var n_match;
+
+    // function to get a random color and apply the *same* color to the background and also get it's name for displaying in the .color span
+    function setColor($color, $div) {
+        $n_match = ntc.name($color);
+        $div.css('background', $color);
+        $div.find('span.color').text($n_match[1]);
+    }
+
     config.on('config-initialized', function(event, data) {
 
-        // For 1st Box
-        $('#group1').css('background', config.preferences['Color 1']);
+        // Loop through all the widget preferences, find the colors, and add them to array
+        var colors = [];
+        for (var preferenceKey in config.preferences) {
+            if(preferenceKey.toLowerCase().indexOf('color') >= 0) {
+                colors.push(config.preferences[preferenceKey]);
+            }
+        }
+        // Get a random number between 0 and 8 (based on length of colors array which is 9 colors)
+        var rand = Math.floor(Math.random()*colors.length);
 
-        // Get name of color preference from hex-code
-        n_match = ntc.name(config.preferences['Color 1']);
-        $('#group1').find('span.color').text(n_match[1]);
+        // Set color of first div
+        setColor(colors[rand], $('#group1'));
 
 
     });
     config.on('config-error', function() {
-        $('#group1').text('Error loading preferences');
+        $('#error').text('Error loading SignageLive widget preferences');
     });
     config.init();
 });
