dcb71d76f4045240d7f98d30532eb11377391580
Adrienne Cabouet <adriennecabouet@Adriennes-MacBook-Pro.local>
2018-01-29 17:16:59 -0700

    Updated config and preferences

--- scripts/app.js
+++ scripts/app.js
@@ -12,27 +12,43 @@ require(['jquery', 'wdf/widget-config', 'ntc'], function($, WidgetConfig) {
     // load the widget configuration
     var config = new WidgetConfig();
 
-    // function to get a random color and apply the *same* color to the background and also get it's name for displaying in the .color span
+    // function to apply a color to the background of a div and also get it's name for displaying in the .color span
     function setColor($color, $div) {
         $n_match = ntc.name($color);
         $div.css('background', $color);
         $div.find('span.color').text($n_match[1]);
     }
 
+
+    // Get a random number 0 to 9;
+    function getRandomNumber(arr) {
+        if($.isArray(arr)) {
+            return Math.floor(Math.random()*arr.length);
+        } else {
+            return Math.floor(Math.random() * 9);
+        }
+    }
+
+
     config.on('config-initialized', function(event, data) {
 
-        // Loop through all the widget preferences, find the colors, and add them to array
+
+        // Loop through all the widget preferences, find the colors, and add them to array. Also get the time interval and save it to a var to work with.
         var colors = [];
+        var timeInterval;
         for (var preferenceKey in config.preferences) {
             if(preferenceKey.toLowerCase().indexOf('color') >= 0) {
                 colors.push(config.preferences[preferenceKey]);
+            } else if(preferenceKey.toLowerCase().indexOf('time') >= 0) {
+                timeInterval = config.preferences[preferenceKey];
             }
         }
-        // Get a random number between 0 and 8 (based on length of colors array which is 9 colors)
-        var rand = Math.floor(Math.random()*colors.length);
 
-        // Set color of first div
-        setColor(colors[rand], $('#group1'));
+        // Get all the divs we're gonna work with, get three random colors, then set them colors
+        $('.time-block').each(function() {
+            rand = getRandomNumber(colors);
+            setColor(colors[rand], $(this));
+        });
 
 
     });
